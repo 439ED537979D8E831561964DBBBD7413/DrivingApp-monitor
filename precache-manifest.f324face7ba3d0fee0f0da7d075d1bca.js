5f09859465da9faacae89c818b8f8392e8ba438e
Daniel Torres <torresestradadaniel@gmail.com>
2018-11-20 10:36:49 -0600

    Updates

---
+++ precache-manifest.f324face7ba3d0fee0f0da7d075d1bca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d91d35c08e68aec859e8",
+    "url": "/static/js/main.d91d35c0.chunk.js"
+  },
+  {
+    "revision": "be8edc0e6582b1115b53",
+    "url": "/static/js/1.be8edc0e.chunk.js"
+  },
+  {
+    "revision": "d91d35c08e68aec859e8",
+    "url": "/static/css/main.5ee99772.chunk.css"
+  },
+  {
+    "revision": "be8edc0e6582b1115b53",
+    "url": "/static/css/1.30a4c8c5.chunk.css"
+  },
+  {
+    "revision": "20cc43534c8e5b5a29ae498d2d9d824f",
+    "url": "/index.html"
+  }
+];
