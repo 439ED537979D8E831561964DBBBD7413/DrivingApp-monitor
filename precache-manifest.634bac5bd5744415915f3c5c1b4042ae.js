9625285684ae7e0dff5eedb0b637d3a8970242fa
Daniel Torres <torresestradadaniel@gmail.com>
2018-11-20 12:09:38 -0600

    Updates

--- precache-manifest.634bac5bd5744415915f3c5c1b4042ae.js
+++ precache-manifest.634bac5bd5744415915f3c5c1b4042ae.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/DrivingApp-monitor/static/js/runtime~main.25ec8e1b.js"
   },
   {
-    "revision": "4b00196c6459c660d8f0",
-    "url": "/DrivingApp-monitor/static/js/main.4b00196c.chunk.js"
+    "revision": "f0ba6599a790f632b390",
+    "url": "/DrivingApp-monitor/static/js/main.f0ba6599.chunk.js"
   },
   {
     "revision": "13efacffcee3d572d334",
     "url": "/DrivingApp-monitor/static/js/1.13efacff.chunk.js"
   },
   {
-    "revision": "4b00196c6459c660d8f0",
+    "revision": "f0ba6599a790f632b390",
     "url": "/DrivingApp-monitor/static/css/main.5ee99772.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/DrivingApp-monitor/static/css/1.30a4c8c5.chunk.css"
   },
   {
-    "revision": "303d65b0098b1e232931b322594daaa6",
+    "revision": "94e7d62c0e359b7e7ee731fb20fc230b",
     "url": "/DrivingApp-monitor/index.html"
   }
 ];
